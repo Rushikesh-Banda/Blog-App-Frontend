d9789f137524ea84f310561a9908f5b2a94654fb
Rushikesh-banda <rushikeshbanda39@gmail.com>
2026-05-13 23:09:46 +0530

    fixed login route

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -16,12 +16,7 @@ export const useAuth = create((set) => ({
       set({ loading: true, error: null });
 
       //make api call
-      const res = await API.post(
-  role === "AUTHOR"
-    ? "/author-api/login"
-    : "/user-api/login",
-  userCredObj
-);
+      const res = await API.post("/user-api/login", userCredObj);
 
       console.log("login response:", res.data); // added debug
 
